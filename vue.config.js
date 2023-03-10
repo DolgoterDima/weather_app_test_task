e471c03fd4bb2db948478d2440142e2b502c11ac
DDolgy <dmitriy.d@filancy.com>
2023-03-10 10:14:11 +0200

    fix: deploy

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,13 @@ const { defineConfig } = require("@vue/cli-service");
 const webpack = require("webpack");
 
 module.exports = defineConfig({
-  publicPath: "dist/vue/index.html",
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/weather-app/" : "/",
+
   lintOnSave: false,
   configureWebpack: {
+    publicPath: process.env.NODE_ENV === "production" ? "/weather-app/" : "/",
+
     plugins: [
       new webpack.DefinePlugin({
         // allow access to process.env from within the vue app
